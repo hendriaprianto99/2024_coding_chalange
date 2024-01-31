df3f4ac0f07e2495803bd43537dd21699eac9a67
Hendri aprianto <hendriaprianto99@gmail.com>
2024-01-31 10:04:33 +0700

    hari ketiga

---
+++ 3_hero.js
@@ -0,0 +1,17 @@
+// hari ke 3
+
+// function hero(bullets, dragons){
+//     //Get Coding!
+//     if(bullets / 2 >= dragons) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+function hero(bullets, dragons){
+    //Get Coding!
+    return bullets / 2 >= dragons;
+}
+
+console.log(hero(10, 5));
