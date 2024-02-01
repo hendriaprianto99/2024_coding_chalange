b865a9cc878ff18e9e39a06a9a96f72d494d9cec
hendriaprianto99 <hendriaprianto99@gmail.com>
2024-02-01 15:14:52 +0700

    hari keenam

---
+++ 6_DNAtoRNA.js
@@ -0,0 +1,7 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.replace(/T/g, "U");
+}
+
+console.log(DNAtoRNA("TTTT"));
+console.log(DNAtoRNA("GCAT"));
