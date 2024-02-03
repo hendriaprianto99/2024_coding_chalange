1b3a93fb5f4feb42f49ae690e8fb0266823bad35
hendriaprianto99 <hendriaprianto99@gmail.com>
2024-02-03 09:21:43 +0700

    hari kesembilan

---
+++ 9_maxmin.js
@@ -0,0 +1,5 @@
+let min = (list) => Math.min(...list);
+let max = (list) => Math.max(...list);
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
